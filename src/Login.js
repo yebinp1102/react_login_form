c97d91711e7401342971b8108e4d67a57e8634b4
yebin Park <yebinp1102@naver.com>
2022-03-10 10:11:46 +0900

    create auth context

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,8 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, useContext } from 'react';
+import AuthContext from "./context/AuthProvider"
 
 const Login = () => {
-
+  const {setAuth} = useContext(AuthContext);
   const userRef = useRef();
   const errRef = useRef();
 
